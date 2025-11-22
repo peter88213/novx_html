f3c7d56241a6e840643b9dd5e72a53a11003c268
peter88213 <petert88213@gmail.com>
2025-11-22 18:08:59 +0100

    Replaced Error with RuntimeError.

--- src/nvhtmlib/html_fop.py
+++ src/nvhtmlib/html_fop.py
@@ -4,7 +4,6 @@ Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/novx_html
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
-from nvlib.novx_globals import Error
 from nvlib.novx_globals import norm_path
 
 
@@ -12,7 +11,7 @@ def read_html_file(filePath):
     """Open a html file being encoded utf-8 or ANSI.
     
     Return the file content in a single string. None in case of error.
-    Raise the "Error" exception in case of error. 
+    Raise the "RuntimeError" exception in case of error. 
     """
     try:
         with open(filePath, 'r', encoding='utf-8') as f:
@@ -23,6 +22,8 @@ def read_html_file(filePath):
             with open(filePath, 'r') as f:
                 content = (f.read())
         except(FileNotFoundError):
-            raise Error(f'{_("File not found")}: "{norm_path(filePath)}".')
+            raise RuntimeError(
+                f'{_("File not found")}: "{norm_path(filePath)}".'
+            )
 
     return content
